e5fede406247cc0fda63f7c4f649248811385ae6
Rafael <rrcamel@hotmail.com>
2022-07-27 02:38:12 -0300

    Cart View

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand">
-          React-Piotti
+          React-PiottiMotor
         </Link>
       </div>
       <div
